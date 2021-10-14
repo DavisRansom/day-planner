272a1fa360a36b3054b5c00e31a647b709c85c6c
Davis Ransom <davisransom.vb@gmail.com>
2021-10-14 10:39:28 -0700

    Added a chck in local storage for todo evenets and save them to local storage

--- Assets/script.js
+++ Assets/script.js
@@ -9,12 +9,23 @@ $(document).ready(function () {
 
     //save input area content to local storage
     $(".save-button").on("click", function () {
-        const time = $(this).attr("id")
-        const todo = $(this).siblings("textarea").val()
-        console.log (todo)
-    })
-    
-    //section to read from local storage 
+        const time = $(this).parent().attr("id");
+        const todo = $(this).siblings("textarea").val();
+        localStorage.setItem(time,todo);
+    });
+
+    //section to read from local storage
+    //I will grab from local storage the time and todo const for each row
+    //for every element of this array do a check in local storage for that key
+    //if there is a value for that key then print into that row's text area
+    for(let row of document.querySelectorAll(".row")){
+        let time=row.id;
+        let todo=localStorage.getItem(time);
+        if(todo){
+            row.querySelector("textarea").value=todo;
+        }
+    }
+   
 
 
 });
