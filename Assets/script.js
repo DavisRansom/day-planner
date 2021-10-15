71d52d8b4dabda6a0980848f24874d50e219006a
Davis Ransom <davisransom.vb@gmail.com>
2021-10-15 11:28:58 -0700

    Modified CSS and cleaned up formatting

--- Assets/script.js
+++ Assets/script.js
@@ -11,21 +11,21 @@ $(document).ready(function () {
     $(".save-button").on("click", function () {
         const time = $(this).parent().attr("id");
         const todo = $(this).siblings("textarea").val();
-        localStorage.setItem(time,todo);
+        localStorage.setItem(time, todo);
     });
 
     //section to read from local storage
     //I will grab from local storage the time and todo const for each row
     //for every element of this array do a check in local storage for that key
     //if there is a value for that key then print into that row's text area
-    for(let row of document.querySelectorAll(".row")){
-        let time=row.id;
-        let todo=localStorage.getItem(time);
-        if(todo){
-            row.querySelector("textarea").value=todo;
+    for (let row of document.querySelectorAll(".row")) {
+        let time = row.id;
+        let todo = localStorage.getItem(time);
+        if (todo) {
+            row.querySelector("textarea").value = todo;
         }
     }
+
+    //create a click event on the that when clicked removes any items in local storage for that row only
    
-
-
 });
