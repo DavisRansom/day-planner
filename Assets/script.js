ad297acf402ad00f12ab724d23f8e025509b5900
Davis Ransom <davisransom.vb@gmail.com>
2021-10-16 13:10:03 -0700

    Formatted index.html file and JavaScript file

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 
     //color coding the hours list
     const date = new Date();
-    $(".jumbotron p + p").text(date.toLocaleDateString("en-US", { weekday: "long", year: "numeric", month: "long", day: "numeric"}));
+    $(".jumbotron p + p").text(date.toLocaleDateString("en-US", { weekday: "long", year: "numeric", month: "long", day: "numeric" }));
     const currentHour = date.getHours();
     const row = $("#hour" + currentHour);
     if (row) $(row).addClass("current");
@@ -19,9 +19,10 @@ $(document).ready(function () {
     //for every element of this array do a check in local storage for that key
     //if there is a value for that key then print into that row's text area
     for (let row of document.querySelectorAll(".row")) {
-    let time = row.id;
-    let todo = localStorage.getItem(time);
-    if (todo) {
-        row.querySelector("textarea").value = todo;
+        let time = row.id;
+        let todo = localStorage.getItem(time);
+        if (todo) {
+            row.querySelector("textarea").value = todo;
+        }
     }
-}})
+})
